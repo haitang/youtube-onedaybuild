f596e42f70c34877b1b0a01759ae56a98032faf5
Hai T <haitang@gmail.com>
2021-02-12 16:09:10 +0800

    initial project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Tang Hai on 2021/2/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
